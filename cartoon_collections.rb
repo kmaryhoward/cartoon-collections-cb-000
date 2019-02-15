255042d28003588f48b4ca90db61d5a3eef64dd2
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-15 01:01:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  b = array + cheese_types
-  b.select { |x| b.count(x) > 1 }.uniq.join(" ")
+  a = array & cheese_types
+  a[0]
 end
